b7af20d7f485db9f0e561f030ea7aa7411306a0c
Ankit Nigam <ankit.nigam@tothenew.com>
2017-12-09 03:25:04 +0530

    all commit

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div><h1>This is my first react compoment</h1></div>
+        )
+    }
+}
+
+export default App;
---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import App from './App'
+import {render} from 'react-dom'
+
+
+console.log('this is ankit nigam')
+
+let app = document.getElementById("app")
+render(<App/>, app);
